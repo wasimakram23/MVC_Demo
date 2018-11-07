bd83ad569bb3d7bbfc86135e67ac8b1b6ac666ca
wasimcse12 <wasimruet12@gmail.com>
2018-11-07 11:29:52 +0600

    new

---
+++ MVC_App/MVC_App/Controllers/RechargeController.cs
@@ -0,0 +1,114 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace MVC_App.Controllers
+{
+    public class RechargeController : Controller
+    {
+       
+        // GET: Recharge
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+       
+        public string upload()
+        {
+            DataTable dt=null;
+            if (Request.Files["FileUpload1"].ContentLength > 0)
+            {
+                string extension = System.IO.Path.GetExtension(Request.Files["FileUpload1"].FileName).ToLower();
+                string query = null;
+                string connString = "";
+                string[] validFileTypes = { ".xls", ".xlsx", ".csv" };
+
+                string path1 = string.Format("{0}/{1}", Server.MapPath("~/Content/Uploads"), Request.Files["FileUpload1"].FileName);
+                if (!Directory.Exists(path1))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/Content/Uploads"));
+                }
+                if (validFileTypes.Contains(extension))
+                {
+                    if (System.IO.File.Exists(path1))
+                    { System.IO.File.Delete(path1); }
+                    Request.Files["FileUpload1"].SaveAs(path1);
+                    if (extension == ".csv")
+                    {
+                        dt = Utility.ConvertCSVtoDataTable(path1);
+                        ViewBag.Data = dt;
+                    }
+                    //Connection String to Excel Workbook  
+                    else if (extension.Trim() == ".xls")
+                    {
+                        connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path1 + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"";
+                        dt = Utility.ConvertXSLXtoDataTable(path1, connString);
+                        ViewBag.Data = dt;
+                    }
+                    else if (extension.Trim() == ".xlsx")
+                    {
+                        connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path1 + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
+                        dt = Utility.ConvertXSLXtoDataTable(path1, connString);
+                        ViewBag.Data = dt;
+                    }
+
+                }
+                else
+                {
+                    ViewBag.Error = "Please Upload Files in .xls, .xlsx or .csv format";
+
+                }
+
+            }
+            return JsonConvert.SerializeObject(dt);
+        }
+
+        public JsonResult Upload_excel()
+        {
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    object[,] obj = null;
+                    int noOfCol = 0;
+                    int noOfRow = 0;
+                    HttpFileCollectionBase file = Request.Files;
+                    if ((file != null) && (file.Count > 0))
+                    {
+                        //string fileName = file.FileName;
+                        //string fileContentType = file.ContentType;
+                        byte[] fileBytes = new byte[Request.ContentLength];
+                        var data = Request.InputStream.Read(fileBytes, 0, Convert.ToInt32(Request.ContentLength));
+                        // var usersList = new List<Users>();
+                        //using (var package = new ExcelPackage())
+                        using (var package = new ExcelPackage(Request.InputStream))
+                        {
+                            var currentSheet = package.Workbook.Worksheets;
+                            var workSheet = currentSheet.First();
+                            noOfCol = workSheet.Dimension.End.Column;
+                            noOfRow = workSheet.Dimension.End.Row;
+                            obj = new object[noOfRow, noOfCol];
+                            obj = (object[,])workSheet.Cells.Value;
+                            
+                        }
+                    }
+                    return Json(new { data = obj, row = noOfRow, col = noOfCol }, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+            }
+            return Json("", JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
